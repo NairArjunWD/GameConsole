29f5ff1a7f7d4da85c9064a10e8e3372a2f71364
Arjun Nair <nair.arjun47@yahoo.com>
2020-11-21 23:37:01 -0800

    Create class in PlayerCharacter
    Create class in Player Character.

---
+++ PlayerCharacter.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GameConsole
+{
+    class PlayerCharacter
+    {
+        public string Name {get; set;}
+
+        public int DaysSinceLastLogin {get; set;}
+        public DateTime Type {get; set;}
+    }
+}
