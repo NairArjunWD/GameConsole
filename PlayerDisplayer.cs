f325ad387503f5ba074d9ce227894a7e511aff15
Arjun Nair <nair.arjun47@yahoo.com>
2020-11-22 22:51:51 -0800

    Add Player Displayer class
    Add PlayerDisplayer class to the cs file.

---
+++ PlayerDisplayer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace GameConsole
+{
+    class PlayerDisplayer
+    {
+        public static void Write (PlayerCharacter player)
+        {
+            
+        }
+    }
+}
